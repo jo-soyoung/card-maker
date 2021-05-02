c49501420a440fbab32ee289275e429a09ad9a78
Soyoung <cookr341@gmail.com>
2021-05-02 16:53:31 +0900

    add firebase

--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -1,11 +1,10 @@
 import firebase from 'firebase';
+import firebaseApp from './firebase.js';
 
-// 앱에서 유저가 로그인/로그아웃을 하는 이런 authenticaiton과 관련된 일을 하는 class
 class AuthService {
   login(providerName) {
-    //아래에 왜 []을 쓰고 ``을 넣는거지?
     const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
-    return firebase.auth().signInWithPopup(authProvider);
+    return firebaseApp.auth().signInWithPopup(authProvider);
   }
 }
 
